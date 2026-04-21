3f8c26cd243ddbac0756dbe9df9bc9e6bc77d1df
JuanSebLopez <jslopezmarquez@unicesar.edu.co>
2026-04-21 00:46:10 -0500

    chore: configure api proxy for netlify and local dev

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    endpoint: "https://www.sistema-venta.somee.com/api/"
+    endpoint: "/api/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    endpoint: "https://www.sistema-venta.somee.com/api/"
+    endpoint: "/api/"
 };
